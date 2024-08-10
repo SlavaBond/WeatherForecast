688256aa25a78dcd23ca0f1992b888cf2ad0551e
Slava Bondareva <86853916+SlavaBond@users.noreply.github.com>
2024-08-10 14:28:52 +0300

    add dynamic temp, condition, icon

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
 // JavaScript source code
+function updateWeather(response) {
+    console.log(response.data);
+    console.log(response.data.condition.icon_url);
+    let currentTemp = document.querySelector("#current-temp");
+    let currentCity = document.querySelector("#city");
+    let currentDescription = document.querySelector("#description");
+    let currentHumidity = document.querySelector("#humidity");
+    let currentWind = document.querySelector("#wind");
+    let currentDate = document.querySelector("#current-date");
+    let currentIcon = document.querySelector("#current-icon");
+    let date = new Date(response.data.time * 1000);
 
-let searchCity = document.querySelector("#search-city");
+    currentDate.innerHTML = updateDate(date);
+    currentIcon.setAttribute("src", response.data.condition.icon_url);
+    currentTemp.innerHTML = Math.round(response.data.temperature.current);
+    currentCity.innerHTML = response.data.city;
+    currentDescription.innerHTML = response.data.condition.description;
+    currentHumidity.innerHTML = `${response.data.temperature.humidity}%`;
+    currentWind.innerHTML = `${response.data.wind.speed}km/h`;
+}
+
+
+function searchCityFunc(city) {
+    //API
+    let key = 'b1047taba00ddd6f7042cee7ac3b7do3';
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}`;
+    axios.get(apiUrl).then(updateWeather);
+}
+
 
 
 function changeCity(event) {
     event.preventDefault();
-    let currentCity = document.querySelector("#city");
-    currentCity.innerHTML = searchCity.value;
-    //updateDate();
+    let searchCity = document.querySelector("#search-city");
+    searchCityFunc(searchCity.value);
 }
+let form = document.querySelector("#form");
+form.addEventListener("submit", changeCity);
 
-function updateDate() {
+
+
+function updateDate(now) {
     let weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-    let now = new Date();
+   
     let hours = now.getHours();
     let minutes = now.getMinutes();
 
@@ -25,9 +55,6 @@ function updateDate() {
     return `${day} ${hours}:${minutes}`
 }
 
-let currentDate = document.querySelector("#current-date");
-currentDate.innerHTML = updateDate();
 
+searchCityFunc("Paris");
 
-let form = document.querySelector("#form");
-form.addEventListener("submit", changeCity);
